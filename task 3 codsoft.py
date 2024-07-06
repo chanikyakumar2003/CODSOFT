d9b9aeee73274086ea8fc4f59e03329230137c04
chanikyakumar2003 <sunkarachanikyakumar@gmail.com>
2024-07-06 20:30:05 +0530

    Create task 3 codsoft.py
    codsoft internship task 3 random password generator

---
+++ task 3 codsoft.py
@@ -0,0 +1,25 @@
+import random
+import string
+
+def generate_password(length):
+    # Define character sets for the password
+    characters = string.ascii_letters + string.digits + string.punctuation
+
+    # Generate a random password
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+def main():
+    try:
+        desired_length = int(input("Enter the desired password length: "))
+        if desired_length <= 0:
+            print("Please enter a positive integer.")
+            return
+
+        generated_password = generate_password(desired_length)
+        print(f"Generated password: {generated_password}")
+    except ValueError:
+        print("Invalid input. Please enter a valid positive integer.")
+
+if __name__ == "__main__":
+    main()
